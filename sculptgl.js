3b1bea247706b7b558ac62cbfbac6ea6b6e6e2c3
Pierre-Antoine Passet <pa@passet.org>
2021-03-02 12:04:35 +0100

    Fixes context lost wording

--- sculptgl.js
+++ sculptgl.js
@@ -527,7 +527,7 @@ SculptGL.prototype = {
   /** WebGL context is lost */
   onContextLost: function ()
   {
-    alert('shit happens : context lost');
+    alert('error : context lost');
   },
 
   /** WebGL context is restored */
